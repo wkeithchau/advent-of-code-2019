ca81bb0482931423604845caf811d72182eafd2e
Keith Chau <wkeith.chau@gmail.com>
2020-03-10 23:17:04 +1030

    Add part2 solution

--- day01-rocket-equation/index.mjs
+++ day01-rocket-equation/index.mjs
@@ -4,5 +4,42 @@ const fuelRequired = mass => {
   return Math.floor(mass / 3) - 2;
 };
 
-const input = getInput(import.meta.url);
-console.log(input);
+const addedFuelRequired = mass => {
+  let fuel = fuelRequired(mass);
+
+  if (fuel > 0) {
+    fuel += addedFuelRequired(fuel);
+  }
+  return Math.max(fuel, 0);
+};
+
+const part1 = () => {
+  const moduleMasses = getInput(import.meta.url);
+
+  let totalFuel = 0;
+  moduleMasses.forEach(mass => {
+    const fuel = fuelRequired(mass);
+    totalFuel += fuel;
+  });
+
+  console.log(
+    `Sum of the fuel requirements for all of the modules on your spacecraft: ${totalFuel}`
+  );
+  return totalFuel;
+};
+
+const part2 = () => {
+  const moduleMasses = getInput(import.meta.url);
+
+  let totalFuel = 0;
+  moduleMasses.forEach(mass => {
+    const fuel = addedFuelRequired(mass);
+    totalFuel += fuel;
+  });
+
+  console.log(`Taking into account the mass of added fuel: ${totalFuel}`);
+  return totalFuel;
+};
+
+part1();
+part2();

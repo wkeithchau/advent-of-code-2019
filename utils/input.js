b80c73182e4613c2bae22f0047b22e9c73568930
Keith Chau <wkeith.chau@gmail.com>
2020-03-11 22:20:51 +1030

    Fix input path resolver

--- utils/input.js
+++ utils/input.js
@@ -1,17 +1,17 @@
 import fs from 'fs'
 import path from 'path'
 
-const ROOTPATH = path.dirname(import.meta.url)
+const PROCESS_PATH = `file://${process.cwd()}`
 
 const readFile = path => {
     return fs.readFileSync(path, 'utf8')
 }
 
-export const getInput = absUrl => {
-    const relativePath = path.relative(ROOTPATH, absUrl)
+export const getInput = (absUrl, seperator) => {
+    const relativePath = path.relative(PROCESS_PATH, absUrl)
     const inputPath = `${path.dirname(relativePath)}/input`
 
     const input = readFile(inputPath)
-    const lines = input.split('\n')
+    const lines = input.split(seperator)
     return lines
 }

2a88eb3a6ec6489ca388b1fa1e498dd00a35dca3
Keith Chau <wkeith.chau@gmail.com>
2020-04-01 00:26:45 +1030

    Add more methods into Map

--- day03-crossed-wires/map.js
+++ day03-crossed-wires/map.js
@@ -11,6 +11,14 @@ class Map {
         this.portPosition[1] = column
     }
 
+    setHead = (headType, head) => {
+        if (headType === symbol.wire1) {
+            this.setWire1Head(...head)
+        } else if (headType === symbol.wire2) {
+            this.setWire2Head(...head)
+        }
+    }
+
     setWire1Head = (row, column) => {
         this.wire1Head[0] = row
         this.wire1Head[1] = column
@@ -19,6 +27,14 @@ class Map {
     setWire2Head = (row, column) => {
         this.wire2Head[0] = row
         this.wire2Head[1] = column
+    }
+
+    show = () => {
+        console.log()
+        this.grid.forEach(row => {
+            console.log(row.join(' '))
+            console.log()
+        })
     }
 
     reset = () => {

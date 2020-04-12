44a88960e6922f9c847f48eb947e62fd51ccae73
Keith Chau <wkeith.chau@gmail.com>
2020-04-12 12:46:14 +0930

    Rework Segment to have direction and steps

--- day03-crossed-wires/segment.js
+++ day03-crossed-wires/segment.js
@@ -1,8 +1,17 @@
+import { orientation } from './constants'
+
+const PLANE = {
+    horizontal: 'horizontal',
+    vertical: 'vertical',
+}
+
 class Segment {
     x1
     x2
     y1
     y2
+    direction
+    initial
 
     constructor(pointA, pointB, direction, steps) {
         this.x1 = pointA.x
@@ -10,7 +19,51 @@ class Segment {
         this.y1 = pointA.y
         this.y2 = pointB.y
         this.direction = direction
-        this.inital = steps
+        this.initial = steps
+    }
+
+    steps = pos => {
+        if (this.direction === undefined || this.initial === undefined) {
+            return
+        }
+
+        let startPos
+        let selectedPos
+        let plane
+        if (this.direction === orientation.left) {
+            startPos = this.x2
+            selectedPos = this.x1
+            plane = PLANE.horizontal
+        } else if (this.direction === orientation.right) {
+            startPos = this.x1
+            selectedPos = this.x2
+            plane = PLANE.horizontal
+        } else if (this.direction === orientation.up) {
+            startPos = this.y1
+            selectedPos = this.y2
+            plane = PLANE.vertical
+        } else if (this.direction === orientation.down) {
+            startPos = this.y2
+            selectedPos = this.y1
+            plane = PLANE.vertical
+        }
+
+        if (pos !== undefined) {
+            selectedPos = pos
+        }
+
+        if (plane === PLANE.horizontal) {
+            if (selectedPos < this.x1 || selectedPos > this.x2) {
+                return
+            }
+        } else if (plane === PLANE.vertical) {
+            if (selectedPos < this.y1 || selectedPos > this.y2) {
+                return
+            }
+        }
+
+        const travelled = Math.abs(Math.abs(startPos) - Math.abs(selectedPos))
+        return this.initial + travelled
     }
 }
 
--- day03-crossed-wires/segment.test.js
+++ day03-crossed-wires/segment.test.js
@@ -1,17 +1,22 @@
 import { expect } from 'chai'
 
+import { orientation } from './constants'
 import Segment from './segment'
 
 describe('Segment Class', function() {
     describe('Constructor', function() {
         let pointA
         let pointB
+        let direction
+        let initial
         let segment
 
         before(function() {
             pointA = { x: 1, y: 3 }
             pointB = { x: 2, y: 4 }
-            segment = new Segment(pointA, pointB)
+            direction = orientation.right
+            initial = 44
+            segment = new Segment(pointA, pointB, direction, initial)
         })
 
         it('Sets up x1 properly', function() {
@@ -27,6 +32,122 @@ describe('Segment Class', function() {
         })
         it('Sets up y2 properly', function() {
             expect(segment.y2).to.equal(pointB.y)
+        })
+
+        it('Sets up direction', function() {
+            expect(segment.direction).to.equal(direction)
+        })
+
+        it('Sets up initial', function() {
+            expect(segment.initial).to.equal(initial)
+        })
+    })
+
+    describe('steps', function() {
+        let pointA
+        let pointB
+        let initial
+
+        before(function() {
+            pointA = { x: 1, y: 2 }
+            pointB = { x: 6, y: 7 }
+            initial = 44
+        })
+
+        it('Returns undefined when direction is not provided', function() {
+            const segment = new Segment(pointA, pointB, undefined, initial)
+            const steps = segment.steps()
+            expect(steps).to.be.undefined
+        })
+
+        it('Returns undefined when initial is not provided', function() {
+            const segment = new Segment(pointA, pointB, orientation.up)
+            const steps = segment.steps()
+            expect(steps).to.be.undefined
+        })
+
+        it('Returns length of segment when no argument is provided', function() {
+            const segment = new Segment(pointA, pointB, orientation.up, initial)
+            const steps = segment.steps()
+            expect(steps).to.equal(initial + 5)
+        })
+
+        it('Returns distance from starting point when travelling left', function() {
+            const segment = new Segment(
+                pointA,
+                pointB,
+                orientation.left,
+                initial
+            )
+            const steps = segment.steps(2)
+            expect(steps).to.equal(initial + 4)
+        })
+
+        it('Returns distance from starting point when travelling right', function() {
+            const segment = new Segment(
+                pointA,
+                pointB,
+                orientation.right,
+                initial
+            )
+            const steps = segment.steps(2)
+            expect(steps).to.equal(initial + 1)
+        })
+
+        it('Returns distance from starting point when travelling up', function() {
+            const segment = new Segment(pointA, pointB, orientation.up, initial)
+            const steps = segment.steps(6)
+            expect(steps).to.equal(initial + 4)
+        })
+
+        it('Returns distance from starting point when travelling down', function() {
+            const segment = new Segment(
+                pointA,
+                pointB,
+                orientation.down,
+                initial
+            )
+            const steps = segment.steps(6)
+            expect(steps).to.equal(initial + 1)
+        })
+
+        it('Returns undefined when specified point is below segment x range', function() {
+            const segment = new Segment(
+                pointA,
+                pointB,
+                orientation.left,
+                initial
+            )
+            const steps = segment.steps(0)
+            expect(steps).to.undefined
+        })
+
+        it('Returns undefined when specified point is above segment x range', function() {
+            const segment = new Segment(
+                pointA,
+                pointB,
+                orientation.right,
+                initial
+            )
+            const steps = segment.steps(7)
+            expect(steps).to.undefined
+        })
+
+        it('Returns undefined when specified point is below segment y range', function() {
+            const segment = new Segment(pointA, pointB, orientation.up, initial)
+            const steps = segment.steps(8)
+            expect(steps).to.undefined
+        })
+
+        it('Returns undefined when specified point is above segment y range', function() {
+            const segment = new Segment(
+                pointA,
+                pointB,
+                orientation.down,
+                initial
+            )
+            const steps = segment.steps(1)
+            expect(steps).to.undefined
         })
     })
 })

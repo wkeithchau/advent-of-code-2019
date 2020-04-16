3b8f91ea2fe41df901d4034898b293f749e1f67e
Keith Chau <wkeith.chau@gmail.com>
2020-04-17 00:31:18 +0930

    Add solution to day5 part1

--- day05-diagnostic-program/index.js
+++ day05-diagnostic-program/index.js
@@ -1,4 +1,12 @@
 import { getInput } from '../utils/input'
+import Computer from './computer'
 
 const INPUT = getInput(import.meta.url, ',')
 const PROGRAM = INPUT.map(data => Number(data))
+
+const part1 = async () => {
+    console.log('part1 - ACU Test')
+    await Computer.run(PROGRAM)
+}
+
+part1()

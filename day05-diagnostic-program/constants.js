c9675eac8e7c4f83b612d3228b98b4fc91b708e4
Keith Chau <wkeith.chau@gmail.com>
2020-04-17 00:29:53 +0930

    Add intercode computer constants

---
+++ day05-diagnostic-program/constants.js
@@ -0,0 +1,24 @@
+export const opcode = {
+    add: 1,
+    multiply: 2,
+    input: 3,
+    output: 4,
+    end: 99,
+}
+
+export const opLength = {
+    add: 4,
+    multiply: 4,
+    input: 2,
+    output: 2,
+    end: 1,
+}
+
+export const parameterMode = {
+    position: 0,
+    immediate: 1,
+}
+
+export const systemId = {
+    acu: 1,
+}

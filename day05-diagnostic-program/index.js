4fa7713c0d8081d2aad4b3a012e5fd59a584e85a
Keith Chau <wkeith.chau@gmail.com>
2020-04-17 01:29:27 +0930

    Clean up day5 runner to guide user input

--- day05-diagnostic-program/index.js
+++ day05-diagnostic-program/index.js
@@ -1,12 +1,17 @@
 import { getInput } from '../utils/input'
+import { systemId } from './constants'
 import Computer from './computer'
 
 const INPUT = getInput(import.meta.url, ',')
 const PROGRAM = INPUT.map(data => Number(data))
 
-const part1 = async () => {
-    console.log('part1 - ACU Test')
+const diagnose = async () => {
+    console.log('Diagnostic Test')
+    console.log(`Part1 - air conditioner unit: system_id=${systemId.acu}`)
+    console.log(
+        `Part2 - thermal radiator controller: system_id=${systemId.trc}`
+    )
     await Computer.run(PROGRAM)
 }
 
-part1()
+diagnose()

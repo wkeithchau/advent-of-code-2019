4805827cbbd6dcf7a06fa4789dd428183e0787ac
Keith Chau <wkeith.chau@gmail.com>
2020-04-17 01:28:49 +0930

    Add constants for new opcodes and system id

--- day05-diagnostic-program/constants.js
+++ day05-diagnostic-program/constants.js
@@ -3,6 +3,10 @@ export const opcode = {
     multiply: 2,
     input: 3,
     output: 4,
+    jumpTrue: 5,
+    jumpFalse: 6,
+    lessThan: 7,
+    equal: 8,
     end: 99,
 }
 
@@ -11,6 +15,10 @@ export const opLength = {
     multiply: 4,
     input: 2,
     output: 2,
+    jumpTrue: 3,
+    jumpFalse: 3,
+    lessThan: 4,
+    equal: 4,
     end: 1,
 }
 
@@ -21,4 +29,5 @@ export const parameterMode = {
 
 export const systemId = {
     acu: 1,
+    trc: 5,
 }

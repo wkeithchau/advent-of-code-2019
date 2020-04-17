e67194b1dc13a3a5aecf93c194d4bb3579229687
Keith Chau <wkeith.chau@gmail.com>
2020-04-18 00:15:30 +0930

    Revert "Revert "Add solution to day6 part2""
    This reverts commit 98a8d904fb65d913839af85b8c8d2014ae6e80b2.

--- day06-orbit-map/map.test.js
+++ day06-orbit-map/map.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 
-import { loadMap, orbitCount } from './map'
+import { object } from './constants'
+import { loadMap, orbitCount, rootPath } from './map'
 
 describe('Day06 - Orbit Map', function() {
     describe('loadMap', function() {
@@ -68,6 +69,60 @@ describe('Day06 - Orbit Map', function() {
             const directory = loadMap(orbits)
             const count = orbitCount(directory)
             expect(count).to.equal(42)
+        })
+    })
+
+    describe('rootPath', function() {
+        it('Traces path to the provided object name', function() {
+            const orbits = [
+                ['A', 'B'],
+                ['B', 'C'],
+                ['C', 'D'],
+            ]
+            const directory = loadMap(orbits)
+            const dest = directory['D']
+            const path = rootPath(dest, 'B')
+            expect(path).to.have.ordered.members(['C', 'B'])
+        })
+    })
+
+    describe('Overall', function() {
+        it('Sample test 1: minimum_orbital_transfers=4', function() {
+            const input = [
+                'COM)B',
+                'B)C',
+                'C)D',
+                'D)E',
+                'E)F',
+                'B)G',
+                'G)H',
+                'D)I',
+                'E)J',
+                'J)K',
+                'K)L',
+                'K)YOU',
+                'I)SAN',
+            ]
+            const orbits = input.map(i => i.split(')'))
+            const directory = loadMap(orbits)
+            const departure = directory[object.departure].parent
+            const arrival = directory[object.arrival].parent
+
+            // Find path to get to COM
+            const departurePath = rootPath(departure, object.com)
+            const arrivalPath = rootPath(arrival, object.com)
+            // Get first node that intersects
+            const intersection = departurePath.filter(obj =>
+                arrivalPath.includes(obj)
+            )
+            const nodeName = intersection[0]
+            // Get orbital transfer count to the common node
+            const d2n = rootPath(departure, nodeName)
+            const a2n = rootPath(arrival, nodeName)
+            // Total orbital transfer count
+            const transfers = d2n.length + a2n.length
+
+            expect(transfers).to.equal(4)
         })
     })
 })

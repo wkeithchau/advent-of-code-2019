18fff11470dc1ac43a9da5ed4bcc650dcba9a9a2
Keith Chau <wkeith.chau@gmail.com>
2020-04-22 02:15:03 +0930

    Add loopAmps + tests

--- day07-amp-circuit/amps.js
+++ day07-amp-circuit/amps.js
@@ -23,3 +23,48 @@ export const seqAmps = async (program, input, logging = false) => {
 
     return outputE[0]
 }
+
+export const loopAmps = async (program, input, logging = false) => {
+    const ampA = new Computer('A')
+    const inputA = [input[0], 0]
+    const ampB = new Computer('B')
+    const inputB = [input[1]]
+    const ampC = new Computer('C')
+    const inputC = [input[2]]
+    const ampD = new Computer('D')
+    const inputD = [input[3]]
+    const ampE = new Computer('E')
+    const inputE = [input[4]]
+
+    // Link up outputs between computers
+    ampA.outputComputer = ampB
+    ampB.outputComputer = ampC
+    ampC.outputComputer = ampD
+    ampD.outputComputer = ampE
+    ampE.outputComputer = ampA
+
+    ampA.run(program, inputA, { logging: logging, loop: true })
+    ampB.run(program, inputB, { logging: logging, loop: true })
+    ampC.run(program, inputC, { logging: logging, loop: true })
+    ampD.run(program, inputD, { logging: logging, loop: true })
+    ampE.run(program, inputE, { logging: logging, loop: true })
+
+    let allEnd = false
+    while (allEnd === false) {
+        allEnd = ampA.end && ampB.end && ampC.end && ampD.end && ampE.end
+        if (allEnd === false) {
+            await new Promise(resolve => {
+                setTimeout(resolve, 5)
+            })
+        }
+    }
+    const outputs = [
+        ...ampA.output,
+        ...ampB.output,
+        ...ampC.output,
+        ...ampD.output,
+        ...ampE.output,
+    ]
+    const max = Math.max(...outputs)
+    return max
+}
--- day07-amp-circuit/amps.test.js
+++ day07-amp-circuit/amps.test.js
@@ -1,22 +1,8 @@
 import { expect } from 'chai'
-import sinon from 'sinon'
 
-import { seqAmps } from './amps'
+import { loopAmps, seqAmps } from './amps'
 
 describe('Day07 - Amp Circuit', function() {
-    let stubs
-
-    before(function() {
-        stubs = []
-    })
-
-    afterEach(function() {
-        while (stubs.length !== 0) {
-            const stub = stubs.shift()
-            stub.restore()
-        }
-    })
-
     describe('seqAmps', function() {
         it('Sample test 1: max=43210 with seq=[4,3,2,1,0]', async function() {
             const program = [
@@ -39,10 +25,6 @@ describe('Day07 - Amp Circuit', function() {
                 0,
             ]
             const seq = [4, 3, 2, 1, 0]
-
-            const consoleStub = sinon.stub(console, 'log')
-            stubs.push(consoleStub)
-
             const result = await seqAmps(program, seq)
             expect(result).to.equal(43210)
         })
@@ -76,10 +58,6 @@ describe('Day07 - Amp Circuit', function() {
                 0,
             ]
             const seq = [0, 1, 2, 3, 4]
-
-            const consoleStub = sinon.stub(console, 'log')
-            stubs.push(consoleStub)
-
             const result = await seqAmps(program, seq)
             expect(result).to.equal(54321)
         })
@@ -122,12 +100,114 @@ describe('Day07 - Amp Circuit', function() {
                 0,
             ]
             const seq = [1, 0, 4, 3, 2]
-
-            const consoleStub = sinon.stub(console, 'log')
-            stubs.push(consoleStub)
-
             const result = await seqAmps(program, seq)
             expect(result).to.equal(65210)
+        })
+    })
+
+    describe('loopAmps', function() {
+        this.timeout(5000)
+
+        it('Sample test 1: max=139629729 with seq=[9,8,7,6,5]', async function() {
+            const program = [
+                3,
+                26,
+                1001,
+                26,
+                -4,
+                26,
+                3,
+                27,
+                1002,
+                27,
+                2,
+                27,
+                1,
+                27,
+                26,
+                27,
+                4,
+                27,
+                1001,
+                28,
+                -1,
+                28,
+                1005,
+                28,
+                6,
+                99,
+                0,
+                0,
+                5,
+            ]
+            const seq = [9, 8, 7, 6, 5]
+            const result = await loopAmps(program, seq)
+            expect(result).to.equal(139629729)
+        })
+
+        it('Sample test 2: max=18216 with seq=[9,7,8,5,6]', async function() {
+            const program = [
+                3,
+                52,
+                1001,
+                52,
+                -5,
+                52,
+                3,
+                53,
+                1,
+                52,
+                56,
+                54,
+                1007,
+                54,
+                5,
+                55,
+                1005,
+                55,
+                26,
+                1001,
+                54,
+                -5,
+                54,
+                1105,
+                1,
+                12,
+                1,
+                53,
+                54,
+                53,
+                1008,
+                54,
+                0,
+                55,
+                1001,
+                55,
+                1,
+                55,
+                2,
+                53,
+                55,
+                53,
+                4,
+                53,
+                1001,
+                56,
+                -1,
+                56,
+                1005,
+                56,
+                6,
+                99,
+                0,
+                0,
+                0,
+                0,
+                10,
+            ]
+            const seq = [9, 7, 8, 5, 6]
+            const result = await loopAmps(program, seq)
+            expect(result).to.equal(18216)
         })
     })
 })

dc2fe08d1f7c349489e0ec8d5ee5d1ad77cf44e4
Keith Chau <wkeith.chau@gmail.com>
2020-04-22 02:15:15 +0930

    Add solution to day7 part2

--- day07-amp-circuit/index.js
+++ day07-amp-circuit/index.js
@@ -1,5 +1,5 @@
 import { getInput } from '../utils/input'
-import { seqAmps } from './amps'
+import { loopAmps, seqAmps } from './amps'
 
 const INPUT = getInput(import.meta.url, ',')
 const PROGRAM = INPUT.map(data => Number(data))
@@ -32,4 +32,35 @@ const part1 = async () => {
     console.log(`Highest signal that can be sent to the thrusters: ${max}`)
 }
 
+const part2 = async () => {
+    const signals = []
+    const phaseSettings = [5, 6, 7, 8, 9]
+    const settingA = phaseSettings.slice()
+    for (let i = 0; i < settingA.length; i += 1) {
+        const phaseA = settingA[i]
+        const settingB = settingA.filter(val => val !== phaseA)
+        for (let j = 0; j < settingB.length; j += 1) {
+            const phaseB = settingB[j]
+            const settingC = settingB.filter(val => val !== phaseB)
+            for (let k = 0; k < settingC.length; k += 1) {
+                const phaseC = settingC[k]
+                const settingD = settingC.filter(val => val !== phaseC)
+                for (let l = 0; l < settingD.length; l += 1) {
+                    const phaseD = settingD[l]
+                    const phaseE = settingD.filter(val => val !== phaseD)[0]
+                    const seq = [phaseA, phaseB, phaseC, phaseD, phaseE]
+                    const signal = await loopAmps(PROGRAM, seq)
+                    signals.push(signal)
+                }
+            }
+        }
+    }
+
+    const max = Math.max(...signals)
+    console.log(
+        `Highest signal that can be sent to the thrusters with feedback loop: ${max}`
+    )
+}
+
 part1()
+part2()

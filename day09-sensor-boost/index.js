39d8990b61d9e16303d937f2f6e27075a12067d5
Keith Chau <wkeith.chau@gmail.com>
2020-04-24 00:40:46 +0930

    Add solution to day9 part2

--- day09-sensor-boost/index.js
+++ day09-sensor-boost/index.js
@@ -6,7 +6,15 @@ const PROGRAM = INPUT.map(data => Number(data))
 
 const part1 = async () => {
     const computer = new Computer('intercode')
-    await computer.run(PROGRAM, [1])
+    const output = await computer.run(PROGRAM, [1], { logging: false })
+    console.log(`BOOST keycode: ${output[0]}`)
+}
+
+const part2 = async () => {
+    const computer = new Computer('intercode')
+    const output = await computer.run(PROGRAM, [2], { logging: false })
+    console.log(`Coordinates of the distress signal: ${output[0]}`)
 }
 
 part1()
+part2()

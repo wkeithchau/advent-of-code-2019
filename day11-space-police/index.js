0fdf585b718a26d4a5f522731c8854e32426762a
Keith Chau <wkeith.chau@gmail.com>
2020-04-27 01:03:11 +0930

    Add solution to day11 part1

--- day11-space-police/index.js
+++ day11-space-police/index.js
@@ -1,4 +1,36 @@
 import { getInput } from '../utils/input'
 
+import Computer from './computer'
+import Map from './map'
+
 const INPUT = getInput(import.meta.url, ',')
 const PROGRAM = INPUT.map(data => Number(data))
+
+const part1 = async () => {
+    const map = new Map()
+    const robot = new Computer('robot')
+    // Link the map and robot together
+    map.outputComputer = robot
+    robot.outputComputer = map
+
+    map.run()
+    robot.run(PROGRAM, [], { loop: true, logging: false })
+
+    let allEnd = false
+    while (allEnd === false) {
+        allEnd = map.end && robot.end
+        if (allEnd === false) {
+            await new Promise(resolve => {
+                setTimeout(resolve, 5)
+            })
+        }
+    }
+
+    const panels = map.panels
+    const panelCount = Object.keys(panels).length
+    console.log(
+        `Number of panels that the robot painted at least once: ${panelCount}`
+    )
+}
+
+part1()
